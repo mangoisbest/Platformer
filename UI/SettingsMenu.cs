7dfa654510721c79397c039bd7514634f50af087
ashcarter <69620839+mangoisbest@users.noreply.github.com>
2022-10-11 10:51:10 +1000

    Create SettingsMenu.cs

---
+++ UI/SettingsMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+  
+   public AudioMixer audioMixer;
+
+}
